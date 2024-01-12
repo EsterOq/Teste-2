64f5ea022fc86040aba508ae325a55ad11382b19
Ester Ferreira <silvaesterferreiraa@gmail.com>
2024-01-11 21:52:14 -0300

    Primeiro commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//Saida de texto
+Console.WriteLine("Hello Word!");
+
+string nome;
+int idade;
+decimal altura;
+
+Console.WriteLine("Qual o seu nome?");
+nome = Console.ReadLine();
+Console.Write("Quantos anos você tem? ");
+idade = int.Parse(Console.ReadLine());
+Console.Write("Qual a sua altura? ");
+altura = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Prazer " + nome + " sua idade é " + idade + " Sua altura é : " + altura);
